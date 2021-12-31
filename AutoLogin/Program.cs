9a0849dbc9c9324e38a4fddaa1ab8bcd5b4f8cfc
Christoffer Ankersen <chriller_2@hotmail.com>
2021-12-31 11:19:55 +0100

    Update packages to newest versions

--- AutoLogin/Program.cs
+++ AutoLogin/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Threading;
 
@@ -9,17 +10,21 @@ namespace AutoLogin
     {
         static void Main(string[] args)
         {
-            const string name = "USERNAME";
-            const string password = "PASSWORD";
+            const string name = "";
+            const string password = "";
             Uri url = new Uri("https://relesys.relesysapp.net");
             Console.ForegroundColor = ConsoleColor.Green;
 
             // Randomize the time to run this
             Random random = new Random();
             int minutesToWait = random.Next(0, 60);
-            int millisecondsToWait = (minutesToWait * 60000) + 1000;
+#if DEBUG
+            minutesToWait = 0;
+#endif
+            int millisecondsToWait = (minutesToWait * 1000) + 1000;
+            TimeSpan timeout = new TimeSpan(0, 0, 30);
 
-            Console.WriteLine("Waiting " + (millisecondsToWait / 60000).ToString() + " minutes before continuing!");
+            Console.WriteLine("Waiting " + (millisecondsToWait / 1000).ToString() + " minutes before continuing!");
             Console.WriteLine("Triggered at: " + DateTime.Now.ToString("HH:mm") + " - Executing at: " + DateTime.Now.AddMinutes(minutesToWait).ToString("HH:mm"));
             Thread.Sleep(millisecondsToWait);
 
@@ -27,6 +32,7 @@ namespace AutoLogin
             {
                 // Open Chrome
                 ChromeDriver chrome = new ChromeDriver();
+                var awaiter = new WebDriverWait(chrome, timeout);
                 chrome.Navigate().GoToUrl(url);
 
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -34,22 +40,20 @@ namespace AutoLogin
                 Console.ForegroundColor = ConsoleColor.Red;
 
                 // Username (if not already filled)
-                IWebElement userField = chrome.FindElementById("Username");
-                userField.Clear();
-                userField.SendKeys(name);
+                IWebElement loginElement = awaiter.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("Username")));
+                loginElement.Clear();
+                loginElement.SendKeys(name);
 
                 // Password
-                IWebElement passwordField = chrome.FindElementById("Password");
+                IWebElement passwordField = awaiter.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("Password")));
                 passwordField.Clear();
                 passwordField.SendKeys(password);
 
                 // Login
-                IWebElement login = chrome.FindElementByName("button");
+                IWebElement login = awaiter.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name("button")));
                 login.Click();
 
-                Thread.Sleep(3000);
-
-                IWebElement skip = chrome.FindElementByCssSelector("button[ng-click='Continue()']");
+                IWebElement skip = awaiter.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("button[ng-click='Continue()']")));
                 skip.Click();
 
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -58,22 +62,19 @@ namespace AutoLogin
                 Console.WriteLine("Responding to workmood!");
                 Console.ForegroundColor = ConsoleColor.Red;
 
-                Thread.Sleep(3000);
 
                 // Toggle burger menu
-                IWebElement toggleMenu = chrome.FindElementByCssSelector("button[ng-click='BaseCtrl.ToggleMenu()']");
+                IWebElement toggleMenu = awaiter.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("button[ng-click='BaseCtrl.ToggleMenu()']")));
                 toggleMenu.Click();
 
-                Thread.Sleep(3000);
+                Thread.Sleep(5000);
 
                 // Find Work Mood module
                 IWebElement workMood = chrome.FindElement(By.XPath("//span[contains(text(),'Work Mood')]"));
                 workMood.Click();
 
-                Thread.Sleep(3000);
-
                 // Press nr. 5
-                IWebElement happy = chrome.FindElement(By.XPath("//img[@src='/Content/Graphics/WorkMood/5.png']"));
+                IWebElement happy = awaiter.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//img[@src='/Content/Graphics/WorkMood/5.png']")));
                 happy.Click();
 
                 Thread.Sleep(2500);
